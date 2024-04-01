c15c1408d4fe0136e48a98da793c6167d37208a5
shyeonn <tkdgus2916@ajou.ac.kr>
2024-04-01 15:15:27 +0900

    Add function from single_cycle_cpu

--- rv32i_pipe.c
+++ rv32i_pipe.c
@@ -103,11 +103,6 @@ int main (int argc, char *argv[]) {
 	while (cc < CLK_NUM) {
 		printf("\n*** CLK : %d ***\n", cc);
 
-
-
-
-
-
 		// Program counter
 		pc_next = pc_curr + 4;
 
@@ -297,6 +292,7 @@ int main (int argc, char *argv[]) {
 		imem_in.addr = pc_curr;
 
 		imem_out = imem(imem_in, imem_data);
+
 		// no more instruction
 		if(!imem_out.dout)
 			break;
@@ -323,4 +319,160 @@ int main (int argc, char *argv[]) {
 	free(dmem_data);
 
 	return 1;
+}
+
+struct imem_output_t imem(struct imem_input_t imem_in, uint32_t *imem_data) {
+	
+	struct imem_output_t imem_out;
+
+	imem_out.dout = imem_data[imem_in.addr/4];
+
+	return imem_out;
+}
+
+struct regfile_output_t regfile(struct regfile_input_t regfile_in, uint32_t *reg_data, enum REG regwrite){
+
+	struct regfile_output_t regfile_out;
+
+	if(regwrite == READ){
+		regfile_out.rs1_dout = reg_data[regfile_in.rs1];
+		if(regfile_in.rs2 < REG_WIDTH)
+			regfile_out.rs2_dout = reg_data[regfile_in.rs2];
+	}
+	else if(regwrite  = WRITE){
+		reg_data[regfile_in.rd] = regfile_in.rd_din;
+	}
+
+	return regfile_out;
+}
+
+struct alu_output_t alu(struct alu_input_t alu_in){
+
+	struct alu_output_t alu_out;
+
+	alu_out.zero = 1;
+	alu_out.sign = 1;
+	alu_out.ucmp = 0;
+
+	switch(alu_in.alu_control){
+		case C_AND:
+			alu_out.result = alu_in.in1 & alu_in.in2;
+			break;
+		case C_OR:
+			alu_out.result = alu_in.in1 | alu_in.in2;
+			break;
+		case C_XOR:
+			alu_out.result = alu_in.in1 ^ alu_in.in2;
+			break;
+		case C_SL:
+			alu_out.result = alu_in.in1 << (int32_t)alu_in.in2;
+			break;
+		case C_SR:
+			alu_out.result = alu_in.in1 >> (int32_t)alu_in.in2;
+			break;
+		case C_SRA:
+			alu_out.result = (int32_t)alu_in.in1 >> (int32_t)alu_in.in2;
+			break;
+		case C_SUB:
+			alu_out.result = (int32_t)alu_in.in1 - (int32_t)alu_in.in2;
+			break;
+		default:
+			alu_out.result = (int32_t)alu_in.in1 + (int32_t)alu_in.in2;
+			break;
+	}
+
+	//does not handle the case of sign = 1 when result is zero
+	if(alu_out.result)
+		alu_out.zero = 0;
+	if((int32_t)alu_out.result > 0)
+		alu_out.sign = 0;
+	if(alu_in.in1 < alu_in.in2)
+		alu_out.ucmp = 1; 
+
+	return alu_out;
+}
+
+uint8_t alu_control_gen(uint8_t opcode, uint8_t func3, uint8_t func7){
+
+	if(opcode == I_L_TYPE || opcode == S_TYPE)
+		return C_ADD;
+	else if(opcode == SB_TYPE)
+		return C_SUB;
+	else {
+		switch(func3){
+		//Arithmetic & Shifts
+			case F3_SL:
+				return C_SL;
+			case F3_ADD_SUB:
+				if(((func7 >> 5) & 1))
+					return C_SUB;
+				else
+					return C_ADD;
+			case F3_SR:
+				if(((func7 >> 5) & 1))
+					return C_SRA;
+				else
+					return C_SR;
+			case F3_XOR:
+				return C_XOR;
+			case F3_OR:
+				return C_OR;
+			case F3_AND:
+				return C_AND;
+			case F3_SLT:
+			case F3_SLTU:
+				return C_SUB;
+		}
+	}
+}
+
+struct dmem_output_t dmem(struct dmem_input_t dmem_in, uint8_t *dmem_data) {
+	struct dmem_output_t dmem_out;
+
+	if(dmem_in.mem_read){
+		switch(dmem_in.func3){
+			case LB:
+			case LBU:
+				dmem_out.dout = dmem_data[dmem_in.addr];
+				//Negative number
+				if(dmem_in.func3 == LB && dmem_out.dout & 0x80)
+					dmem_out.dout |= 0xFFFFFF00;
+				break;
+			case LH:
+			case LHU:
+				dmem_out.dout = dmem_data[dmem_in.addr];
+				dmem_out.dout |= dmem_data[dmem_in.addr+1] << BYTE_BIT;
+				//Negative number
+				if(dmem_in.func3 == LH && dmem_out.dout & 0x8000)
+					dmem_out.dout |= 0xFFFF0000;
+				break;
+			case LW:
+				dmem_out.dout = dmem_data[dmem_in.addr];
+				dmem_out.dout |= dmem_data[dmem_in.addr+1] << BYTE_BIT;
+				dmem_out.dout |= dmem_data[dmem_in.addr+2] << BYTE_BIT*2;
+				dmem_out.dout |= dmem_data[dmem_in.addr+3] << BYTE_BIT*3;
+				break;
+		}
+				D_PRINTF("MEM", "Read : %x", dmem_out.dout);
+	}
+	if(dmem_in.mem_write){
+		switch(dmem_in.func3){
+			case SB:
+				dmem_data[dmem_in.addr] = (uint8_t)dmem_in.din;
+				break;
+			case SH:
+				dmem_data[dmem_in.addr] = (uint8_t)dmem_in.din;
+				dmem_data[dmem_in.addr+1] = (uint8_t)(dmem_in.din >> BYTE_BIT);
+				break;
+			case SW:
+				dmem_data[dmem_in.addr] = (uint8_t)dmem_in.din;
+				dmem_data[dmem_in.addr+1] = (uint8_t)(dmem_in.din >> BYTE_BIT);
+				dmem_data[dmem_in.addr+2] = (uint8_t)(dmem_in.din >> BYTE_BIT*2);
+				dmem_data[dmem_in.addr+3] = (uint8_t)(dmem_in.din >> BYTE_BIT*3);
+				break;
+		}
+				D_PRINTF("MEM", "Write : %08X", *(uint32_t*)(&dmem_data[dmem_in.addr]));
+	}
+
+	return dmem_out;
 }
